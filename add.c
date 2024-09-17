ae9187fcdb79fe300ebe1d9a44bba7e3d647d895
Exeboss02 <gabriel.abr.wik@outlook.com>
2024-09-17 20:05:34 +0200

    freaky aaaah solution

--- add.c
+++ add.c
@@ -3,9 +3,32 @@
 
 int main(int argc, char *argv[])
 {
-  int a = atoi(argv[1]);
-  int b = atoi(argv[2]);
+  long aValue = 0;
+  long bValue = 0;
 
-  printf("%i\n", (int)a + (int)b);
-  return (int)(a + b);
+  if (argc > 1)
+  {
+    char *endptrA;
+    char *endptrB;
+
+    aValue = strtol(argv[1], &endptrA, 10);
+    bValue = strtol(argv[2], &endptrB, 10);
+
+    if(*endptrA == '\0' && *endptrB == '\0')
+    {
+        printf("%i\n", atoi(argv[1]) + atoi(argv[2]));
+    }
+    else
+    {
+      return -1;
+    }
+  }
+
+  //int a = atoi(argv[1]);
+  //int b = atoi(argv[2]);
+
+
+  //printf("%i\n", a);
+  //printf("%i\n", b);
+  return 0;
 }
